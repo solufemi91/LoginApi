b5d1ef3cca9e5e1fd017da3c80d0cdf2a5a17708
Steve Olufemi <solufemi-olukanni@hotmail.com>
2020-05-18 17:29:21 +0100

    created method that returns week object

--- TestClient/Program.cs
+++ TestClient/Program.cs
@@ -10,10 +10,18 @@ namespace TestClient
     {
         static void Main(string[] args)
         {
-          
-            Console.WriteLine("Hello World!");
-            var obj = new Program();
-            var result =  obj.CallClientAsync().Result;
+
+            //Console.WriteLine("Hello World!");
+            //var obj = new Program();
+            //var result =  obj.CallClientAsync().Result;
+
+            // generate the first week by putting 1st of May in the 5th index position
+            //int dayDateNumber = 0;
+
+            var result = GenerateWeeks();
+
+
+
         }
 
         public async Task<IEnumerable<BookingDetailsDTO>> CallClientAsync()
@@ -25,14 +33,54 @@ namespace TestClient
             return result;
         }
 
-        
+        public static Week GenerateWeek(DateTime firstDayOfAMonth)
+        {
+            int[] days = new int[7];
+            int indexPositionOfDate = 0;
+            for (int dayDateNumber = 1; indexPositionOfDate < 6; dayDateNumber++)
+            {
+                DateTime now = DateTime.Now;
+
+                DateTime date = new DateTime(now.Year, now.Month, dayDateNumber);
+
+                indexPositionOfDate = ((int)date.DayOfWeek == 0) ? 6 : (int)date.DayOfWeek - 1;
+
+                days[indexPositionOfDate] = dayDateNumber;
+            }
+
+            var week = new Week
+            {
+                Days = days
+            };
+
+            return week;
+        }
+
+
     }
 
-    public static class Test
+
+
+    public class Calendar
     {
-        public static int MyFirstMethod()
-        {
-            return 1;
-        }
+        public List<string> DaysOfTheWeek { get; set; }
+
+        public List<Year> Years { get; set; }
     }
+
+    public class Year
+    {
+        public List<Month> Months { get; set; }
+    }
+
+    public class Month
+    {
+        public List<Week> Weeks { get; set; }
+    }
+
+    public class Week
+    {
+        public int[] Days { get; set; }
+    }
+
 }
